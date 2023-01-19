eeef6fecc61cf439b428770eaf02bc641be6002d
Lucas Batista <lucasq04@gmail.com>
2023-01-18 22:54:55 -0300

    Printing possible moves

--- src/application/UI.java
+++ src/application/UI.java
@@ -34,7 +34,7 @@ public class UI {
 		System.out.print("\033[H\033[2J");
 		System.out.flush();
 	}
-	
+
 	public static ChessPosition readChessPosition(Scanner sc) {
 		try {
 			String s = sc.nextLine();
@@ -42,7 +42,7 @@ public class UI {
 			int row = Integer.parseInt(s.substring(1));
 
 			return new ChessPosition(column, row);
-			
+
 		} catch (RuntimeException e) {
 			throw new InputMismatchException("Error reading ChessPosition. Valid values are from a1 at h8");
 		}
@@ -52,7 +52,7 @@ public class UI {
 		for (int i = 0; i < pieces.length; i++) {
 			System.out.print((8 - i) + " ");
 			for (int j = 0; j < pieces.length; j++) {
-				printPiece(pieces[i][j]);
+				printPiece(pieces[i][j], false);
 			}
 			System.out.println();
 		}
@@ -60,7 +60,22 @@ public class UI {
 		System.out.println("  a b c d e f g h");
 	}
 
-	private static void printPiece(ChessPiece piece) {
+	public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
+		for (int i = 0; i < pieces.length; i++) {
+			System.out.print((8 - i) + " ");
+			for (int j = 0; j < pieces.length; j++) {
+				printPiece(pieces[i][j], possibleMoves[i][j]);
+			}
+			System.out.println();
+		}
+
+		System.out.println("  a b c d e f g h");
+	}
+
+	private static void printPiece(ChessPiece piece, boolean background) {
+		if (background)
+			System.out.print(ANSI_BLUE_BACKGROUND);
+
 		if (piece == null) {
 			System.out.print("-" + ANSI_RESET);
 		} else {
